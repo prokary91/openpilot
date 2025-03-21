6387df19babeccc1d6eefbd0b4901d9b159b0982
Shane Smiskol <shane@smiskol.com>
2025-03-20 22:21:22 -0700

    not now

--- selfdrive/debug/max_lat_accel.py
+++ selfdrive/debug/max_lat_accel.py
@@ -69,7 +69,7 @@ def find_events(lr: LogReader, qlog: bool = False) -> list[Event]:
       requesting_max = 0
 
       factor = 1 / abs(CO.actuatorsOutput.torque)
-      factor *= np.interp(v_ego, [9, 17], [1.4, 1])
+      # factor *= np.interp(v_ego, [9, 17], [1.4, 1])
 
       # TODO: would we extrapolate on curvature or post roll compensated lateral accel?
       current_lateral_accel = (curvature * v_ego ** 2 - roll * EARTH_G) * factor
