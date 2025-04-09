368611ca343640ce878972eb9ad1cac5f11ff4c9
Maxime Desroches <desroches.maxime@gmail.com>
2025-04-08 21:12:23 -0700

    fix

--- scripts/cam.sh
+++ scripts/cam.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+cleanup() {
+  pkill -9 -f compressed_vipc.py
+  pkill -9 -f watch3
+  ssh comma@192.168.61.123 'pkill -f -9 bridge'
+  ssh comma@192.168.61.123 'pkill -f -9 encoderd'
+  ssh comma@192.168.61.123 'pkill -f -9 camerad'
+  pkill -9 -f '/data/cam.sh'
+}
+
+trap cleanup SIGINT
+
 (
+  ssh comma@192.168.61.123 'nohup /data/cam.sh &' &
+
   cd /home/batman/openpilot/tools/camerastream
   ./compressed_vipc.py 192.168.61.123 --cam $1 &
 
@@ -9,5 +22,3 @@
 
   wait
 );
-
-trap 'pkill -9 -f compressed_vipc.py; pkill -9 -f watch3; exit 1' SIGINT
